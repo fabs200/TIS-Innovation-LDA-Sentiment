6e82e3552c657107dd0b44b8ad1386743977a02d
fabs200 <fabs200@users.noreply.github.com>
2020-05-02 14:37:49 +0200

    added file, updates df_complete by new article_NN_for_lda without need to run ProcessLongfiles.py

---
+++ python/UpdateLongFile.py
@@ -0,0 +1,30 @@
+### Update Long file only by newly preprocessed articles_NN_for_lda
+import pandas
+from python.ConfigUser import path_processedarticles
+from python.params import params as p
+
+# unpack POStag type
+POStag_type = p['POStag_type']
+
+# Read in article long file
+df_long_arti = pandas.read_csv(path_processedarticles + 'csv/articles_for_lda_{}_l.csv'.format(POStag_type),
+                               sep='\t', na_filter=False)
+
+# Read in exported long file from ProcessLongfiles.py
+df_long_complete = pandas.read_csv(path_processedarticles + 'csv/complete_for_lda_{}_l.csv'.format(POStag_type),
+                                   sep='\t', na_filter=False)
+
+# Drop old column
+df_long_complete = df_long_complete.drop(columns=['articles_{}_for_lda'.format(POStag_type)])
+
+# Update column from df_long_arti
+df_long_complete = pandas.merge(df_long_complete, df_long_arti[['Art_ID', 'articles_{}_for_lda'.format(POStag_type)]],
+                                how='inner', on=['Art_ID'], indicator=True)
+
+### Export longfile to csv (will be read in later)
+print('Save upaded final df_long_complete')
+df_long_complete.to_csv(path_processedarticles + 'csv/complete_for_lda_{}_l.csv'.format(POStag_type),
+                        sep='\t', index=False)
+df_long_complete.to_excel(path_processedarticles + 'complete_for_lda_{}_l.xlsx'.format(POStag_type))
+
+###
