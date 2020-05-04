56e51214c9b3e0b3c80714e2b8677a3ab39e0891
fabs200 <fabs200@users.noreply.github.com>
2020-05-04 20:03:56 +0200

    included sentiscore_median for plots

--- python/DescriptivesLDA.py
+++ python/DescriptivesLDA.py
@@ -2,6 +2,7 @@ import pandas
 from python.ConfigUser import path_processedarticles
 from python.params import params as p
 import matplotlib.pyplot as plt
+import numpy as np
 
 # Todo: look up/implement LDAvis
 # https://www.machinelearningplus.com/nlp/topic-modeling-visualization-how-to-present-results-lda-models/
@@ -15,25 +16,26 @@ df_long = pandas.read_csv(path_processedarticles + 'csv/lda_results_{}_l.csv'.fo
                           sep='\t', na_filter=False)
 
 # Select articles
-df_long = df_long[df_long['Art_unique'] == 1][['DomTopic_arti_arti_id',
-                                               'year', 'sentiscore_mean']]
+df_long = df_long[df_long['Art_unique'] == 1][['DomTopic_arti_arti_id', 'year',
+                                               'sentiscore_mean', 'sentiscore_median']]
 # to numeric
 df_long['sentiscore_mean'] = pandas.to_numeric(df_long['sentiscore_mean'], errors='coerce')
+df_long['sentiscore_median'] = pandas.to_numeric(df_long['sentiscore_median'], errors='coerce')
 
-df = df_long.groupby(['DomTopic_arti_arti_id', 'year'])['sentiscore_mean'].median().reset_index()
+df = df_long.groupby(['DomTopic_arti_arti_id', 'year'])[['sentiscore_mean', 'sentiscore_median']].mean().reset_index()
 
 # to integer
 df['DomTopic_arti_arti_id'] = pandas.to_numeric(df['DomTopic_arti_arti_id'], errors='coerce')
 df['DomTopic_arti_arti_id'] = df['DomTopic_arti_arti_id'].astype(int)
 
-line_chart0 = plt.plot(df[df['DomTopic_arti_arti_id']==0]['year'].to_list(), df[df['DomTopic_arti_arti_id']==0]['sentiscore_mean'].to_list(),'--')
-line_chart1 = plt.plot(df[df['DomTopic_arti_arti_id']==1]['year'].to_list(), df[df['DomTopic_arti_arti_id']==1]['sentiscore_mean'].to_list(),'-')
-line_chart2 = plt.plot(df[df['DomTopic_arti_arti_id']==2]['year'].to_list(), df[df['DomTopic_arti_arti_id']==2]['sentiscore_mean'].to_list(),'-.')
-line_chart3 = plt.plot(df[df['DomTopic_arti_arti_id']==3]['year'].to_list(), df[df['DomTopic_arti_arti_id']==3]['sentiscore_mean'].to_list(),'-')
-# line_chart4 = plt.plot(df[df['DomTopic_arti_arti_id']==4]['year'].to_list(), df[df['DomTopic_arti_arti_id']==4]['sentiscore_mean'].to_list(),'--')
-# line_chart5 = plt.plot(df[df['DomTopic_arti_arti_id']==5]['year'].to_list(), df[df['DomTopic_arti_arti_id']==5]['sentiscore_mean'].to_list(),'--')
-# line_chart6 = plt.plot(df[df['DomTopic_arti_arti_id']==6]['year'].to_list(), df[df['DomTopic_arti_arti_id']==6]['sentiscore_mean'].to_list(),'--')
-# line_chart7 = plt.plot(df[df['DomTopic_arti_arti_id']==7]['year'].to_list(), df[df['DomTopic_arti_arti_id']==7]['sentiscore_mean'].to_list(),'--')
+line_chart0 = plt.plot(df[df['DomTopic_arti_arti_id']==0]['year'].to_list(), df[df['DomTopic_arti_arti_id']==0]['sentiscore_mean'].to_list(), '--')
+line_chart1 = plt.plot(df[df['DomTopic_arti_arti_id']==1]['year'].to_list(), df[df['DomTopic_arti_arti_id']==1]['sentiscore_mean'].to_list(), '-')
+line_chart2 = plt.plot(df[df['DomTopic_arti_arti_id']==2]['year'].to_list(), df[df['DomTopic_arti_arti_id']==2]['sentiscore_mean'].to_list(), '-.')
+line_chart3 = plt.plot(df[df['DomTopic_arti_arti_id']==3]['year'].to_list(), df[df['DomTopic_arti_arti_id']==3]['sentiscore_mean'].to_list(), ':')
+# line_chart4 = plt.plot(df[df['DomTopic_arti_arti_id']==4]['year'].to_list(), df[df['DomTopic_arti_arti_id']==4]['sentiscore_mean'].to_list(), '--')
+# line_chart5 = plt.plot(df[df['DomTopic_arti_arti_id']==5]['year'].to_list(), df[df['DomTopic_arti_arti_id']==5]['sentiscore_mean'].to_list(), '--')
+# line_chart6 = plt.plot(df[df['DomTopic_arti_arti_id']==6]['year'].to_list(), df[df['DomTopic_arti_arti_id']==6]['sentiscore_mean'].to_list(), '--')
+# line_chart7 = plt.plot(df[df['DomTopic_arti_arti_id']==7]['year'].to_list(), df[df['DomTopic_arti_arti_id']==7]['sentiscore_mean'].to_list(), '--')
 plt.title('Topics with sentiment over time')
 plt.xlabel('year')
 plt.ylabel('sentiment score')
