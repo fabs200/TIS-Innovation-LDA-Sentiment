ed7251c4cca8c7fc4b79dd1b528a755eee52f5c1
fabs200 <fabs200@users.noreply.github.com>
2020-05-09 21:48:53 +0200

    added bar plot by publishers

--- python/LDAPlots.py
+++ python/LDAPlots.py
@@ -34,8 +34,10 @@ os.makedirs(path_project + "graph/model_{}".format(p['currmodel']), exist_ok=Tru
 print('Loading lda_results_{}_l.csv'.format(p['currmodel']))
 df_long = pandas.read_csv(path_data + 'csv/lda_results_{}_l.csv'.format(p['currmodel']), sep='\t', na_filter=False)
 
-# Select articles
-df_long = df_long[df_long['Art_unique'] == 1][['DomTopic_arti_arti_id', 'year', 'quarter', 'month', 'sentiscore_mean']]
+# Select articles and columns
+df_long = df_long[df_long['Art_unique'] == 1][['DomTopic_arti_arti_id',
+                                               'year', 'quarter', 'month',
+                                               'Newspaper', 'sentiscore_mean']]
 
 # convert dtypes
 df_long['month'] = pandas.to_datetime(df_long['month'], format='%Y-%m')
@@ -233,6 +235,122 @@ plt.title('Absolute frequency of articles with sentiment score over time, by top
                                                                              p['no_below'], p['no_above']))
 
 plt.savefig(path_project + 'graph/model_{}/03_absfreqArt_bytopic_y.png'.format(p['currmodel']))
+
+
+"""
+Graph 4: Frequency analysis, publisher bias, over time (Mejia)
+"""
+
+# groupby Newspapyer, year, aggregate and rename, over time
+df_aggr_publisher_y = df_long[['year', 'Newspaper', 'sentiscore_mean']]\
+    .groupby(['year', 'Newspaper'])\
+    .agg({'Newspaper': 'count', 'sentiscore_mean': ['mean', 'count', 'std']}).reset_index()
+df_aggr_publisher_y.columns = df_aggr_publisher_y.columns.map('_'.join)
+
+# groupby Newspapyer, year, aggregate, drop Newspaper with less than 7 count, sort
+df_aggr_publisher = df_aggr_publisher_y.groupby(['Newspaper_']).\
+    agg({'Newspaper_': 'count',
+         'sentiscore_mean_mean': ['mean', 'std'],
+         'sentiscore_mean_std': 'mean'}).reset_index()
+df_aggr_publisher.columns = df_aggr_publisher.columns.map('_'.join)
+df_aggr_publisher = df_aggr_publisher[df_aggr_publisher['Newspaper__count']>5]
+
+# replace everything in brackets from Newspaper
+df_aggr_publisher['Newspaper__'] = df_aggr_publisher.Newspaper__.\
+    replace(to_replace='\([^)]*\)', value='', regex=True).\
+    str.strip()
+
+# Plot Publisher's sentiment score, with Stderr
+ax = df_aggr_publisher[['Newspaper__', 'sentiscore_mean_mean_mean']].\
+    sort_values(by='sentiscore_mean_mean_mean').\
+    plot(kind='barh', figsize=(7, 7.5), zorder=2, width=0.8)
+ax.tick_params(axis="both", which="both", bottom="off", top="on", labelbottom="off",
+               left="off", right="off", labelleft="on")
+# set up ytitlelabels
+ax.set_yticklabels(df_aggr_publisher.Newspaper__)
+# set up xticks and xtickslabels
+ax.set_xticks(np.around(np.arange(-.6, .81, step=0.2), decimals=1))
+ax.set_xticklabels(np.around(np.arange(-.6, .81, step=0.2), decimals=1))
+# Draw vertical axis lines
+for tick in ax.get_xticks():
+    ax.axvline(x=tick, linestyle='solid', alpha=0.25, color='#eeeeee', zorder=1)
+# Draw horizontal axis lines
+for tick in ax.get_yticks():
+    ax.axhline(y=tick, linestyle='solid', alpha=0.25, color='#eeeeee', zorder=1)
+# no legend
+ax.get_legend().remove()
+# For each bar: Place a label
+for rect in ax.patches:
+    # Get X and Y placement of label from rect.
+    x_value = rect.get_width()
+    y_value = rect.get_y() + rect.get_height() / 2
+
+    # Number of points between bar and label. Change to your liking.
+    space = 5
+    # Vertical alignment for positive values
+    ha = 'left'
+
+    # If value of bar is negative: Place label left of bar
+    if x_value < 0:
+        # Invert space to place label to the left
+        space *= -1
+        # Horizontally align label at right
+        ha = 'right'
+
+    # Use X value as label and format number with one decimal place
+    label = "{:.2f}".format(x_value)
+
+    # Create annotation
+    plt.annotate(
+        label,                      # Use `label` as label
+        (x_value, y_value),         # Place label at end of the bar
+        xytext=(space, 0),          # Horizontally shift label by `space`
+        textcoords="offset points", # Interpret `xytext` as offset in points
+        va='center',                # Vertically center label
+        ha=ha)                      # Horizontally align label differently for
+                                    # positive and negative values.
+
+ax.set_xlabel("Sentiment score")
+plt.title('Average sentiment score by main publishers\n'
+          'POStag: {}, frequency: yearly,\nno_below: {}, no_above: {}'.format(p['POStag'],
+                                                                              p['no_below'], p['no_above']))
+plt.tight_layout()
+plt.show()
+
+plt.savefig(path_project + 'graph/model_{}/04a_sentiscore_bypublisher.png'.format(p['currmodel']))
+
+
+
+# Plot Publisher's sentiment score, with Stderr
+ax = df_aggr_publisher[['sentiscore_mean_mean_mean', 'sentiscore_mean_mean_std']].\
+    sort_values(by='sentiscore_mean_mean_mean').\
+    plot.barh(figsize=(7, 7.75), zorder=2, width=0.65, xerr='sentiscore_mean_mean_std')
+ax.tick_params(axis="both", which="both", bottom="off", top="on", labelbottom="off",
+               left="off", right="off", labelleft="on")
+# set up ytitlelabels
+ax.set_yticklabels(df_aggr_publisher.Newspaper__)
+# set up xticks and xtickslabels
+ax.set_xticks(np.around(np.arange(-.8, .81, step=0.2), decimals=1))
+ax.set_xticklabels(np.around(np.arange(-.8, .81, step=0.2), decimals=1))
+# Draw vertical axis lines
+vals = ax.get_xticks()
+for tick in vals:
+    ax.axvline(x=tick, linestyle='solid', alpha=0.25, color='#eeeeee', zorder=1)
+# no legend
+ax.get_legend().remove()
+ax.set_xlabel("Sentiment score")
+plt.title('Average sentiment score of publisher\n'
+          'POStag: {}, frequency: yearly,\nno_below: {}, no_above: {}'.format(p['POStag'],
+                                                                              p['no_below'], p['no_above']))
+plt.tight_layout()
+plt.show()
+
+plt.savefig(path_project + 'graph/model_{}/04b_absfreqArt_bytopic_y.png'.format(p['currmodel']))
+
+
+
+
+
 
 
 #####
